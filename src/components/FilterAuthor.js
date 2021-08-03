2582aea9fdb196bda5929abcdc7d567366c89bd0
KumarAmitt <amitt2015@outlook.com>
2021-08-04 00:12:45 +0530

    Implement is Author loading to component

--- src/components/FilterAuthor.js
+++ src/components/FilterAuthor.js
@@ -2,13 +2,16 @@ import PropTypes from 'prop-types';
 import uniqid from 'uniqid';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAuthors, getRandomAuthors, loadAuthors } from '../store/slicers/author';
+import {
+  getAuthors, getRandomAuthors, isAuthorLoading, loadAuthors,
+} from '../store/slicers/author';
 import { loadPoetryByAuthor } from '../store/slicers/poetryHub';
 
 const FilterAuthor = ({ filterAuthor }) => {
   const dispatch = useDispatch();
   const authors = useSelector(getAuthors);
   const randomAuthors = useSelector(getRandomAuthors);
+  const isAuthorLoaded = useSelector(isAuthorLoading);
 
   useEffect(() => {
     dispatch(loadAuthors());
@@ -23,17 +26,23 @@ const FilterAuthor = ({ filterAuthor }) => {
   return (
     <>
       <h1>Filter Author</h1>
-      <select onChange={(e) => filterAuthor(e.target.value)}>
-        <option selected disabled>--select by Author--</option>
-        {
-          authors.map((a) => (<option key={uniqid()} value={a}>{a}</option>))
-        }
-      </select>
-      <div>
-        {
-          randomAuthors.map((a) => (<button type="button" key={uniqid()} onClick={() => handleClick(a)}>{a}</button>))
-        }
-      </div>
+      {
+        isAuthorLoaded ? <h2>Author Loading ...</h2> : (
+          <div>
+            <select onChange={(e) => filterAuthor(e.target.value)}>
+              <option selected disabled>--select by Author--</option>
+              {
+              authors.map((a) => (<option key={uniqid()} value={a}>{a}</option>))
+            }
+            </select>
+            <div>
+              {
+              randomAuthors.map((a) => (<button type="button" key={uniqid()} onClick={() => handleClick(a)}>{a}</button>))
+            }
+            </div>
+          </div>
+        )
+      }
     </>
   );
 };

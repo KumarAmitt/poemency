24fe43706662ca19ac2df119541df5693d6d98f8
KumarAmitt <amitt2015@outlook.com>
2021-08-05 20:38:43 +0530

    Create AbsTitle action creator and display the result on select

--- src/components/FilterTitle.js
+++ src/components/FilterTitle.js
@@ -2,17 +2,29 @@ import React, { useEffect } from 'react';
 import Select from 'react-select';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import uniqid from 'uniqid';
+// import { Link } from 'react-router-dom';
 import { getTitles, isTitleLoading, loadTitles } from '../store/slicers/title';
 import Loading from './Loading';
+import { getAbsPoetry, loadPoetryByAbsTitle } from '../store/slicers/poetryHub';
 
 const FilterTitle = ({ filterTitle }) => {
   const dispatch = useDispatch();
   const titles = useSelector(getTitles);
   const isTitlesLoading = useSelector(isTitleLoading);
 
+  const poetry = useSelector(getAbsPoetry);
+  // const isPoetryLoading = useSelector(isPoetryHubLoading);
+
   useEffect(() => {
     dispatch(loadTitles());
   }, []);
+
+  const handleChange = (t) => {
+    dispatch(loadPoetryByAbsTitle(t));
+  };
+
+  console.log(poetry);
 
   return (
     <>
@@ -20,11 +32,29 @@ const FilterTitle = ({ filterTitle }) => {
         isTitlesLoading ? <Loading /> : (
           <Select
             options={titles.map((t) => ({ label: t, value: t }))}
-            onChange={(e) => filterTitle(e.label)}
+            onChange={(e) => filterTitle(e.value)}
             placeholder="Select by Title"
           />
         )
       }
+      <Select
+        options={titles.map((t) => ({ label: t, value: t }))}
+        onChange={(e) => handleChange(e.value)}
+        placeholder="Select by Title"
+      />
+      <div>
+        {' '}
+        {
+        poetry.map((p) => (
+          <div key={uniqid()}>
+            <h5>{p.title}</h5>
+            <h6>{p.author}</h6>
+            <h6>{p.linecount}</h6>
+            {/* <Link to={`/poetry/${absPoetry.author}/${absPoetry.title}`}>Details</Link> */}
+          </div>
+        ))
+      }
+      </div>
     </>
   );
 };
--- src/store/slicers/poetryHub.js
+++ src/store/slicers/poetryHub.js
@@ -7,6 +7,7 @@ const poetryHubSlice = createSlice({
   name: 'poetryHub',
   initialState: {
     poems: [],
+    absPoems: [],
     loading: false,
   },
   reducers: {
@@ -17,6 +18,10 @@ const poetryHubSlice = createSlice({
       poetryHub.poems = action.payload;
       poetryHub.loading = false;
     },
+    absPoemsReceived: (poetryHub, action) => {
+      poetryHub.absPoems = action.payload;
+      poetryHub.loading = false;
+    },
     poemsRequestFailed: (poetryHub, action) => {
       poetryHub .loading = false;
     },
@@ -24,7 +29,9 @@ const poetryHubSlice = createSlice({
 });
 /* eslint-enable */
 
-const { poemsRequested, poemsReceived, poemsRequestFailed } = poetryHubSlice.actions;
+const {
+  poemsRequested, poemsReceived, poemsRequestFailed, absPoemsReceived,
+} = poetryHubSlice.actions;
 export default poetryHubSlice.reducer;
 
 // ACTION CREATOR
@@ -50,11 +57,23 @@ export const loadPoetryByTitle = (title) => apiCallBegan({
   onError: poemsRequestFailed.type,
 });
 
+export const loadPoetryByAbsTitle = (title) => apiCallBegan({
+  url: `/title/${title}:abs/author,title,linecount`,
+  onStart: poemsRequested.type,
+  onSuccess: absPoemsReceived.type,
+  onError: poemsRequestFailed.type,
+});
+
 // SELECTOR
 
 export const getPoetryHub = createSelector(
   (state) => state.entities.poetryHub.poems,
   (poems) => poems,
+);
+
+export const getAbsPoetry = createSelector(
+  (state) => state.entities.poetryHub.absPoems,
+  (absPoems) => absPoems,
 );
 
 export const isPoetryHubLoading = createSelector(

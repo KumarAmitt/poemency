bb90fbfac69ad725fc99d830f8109bb238e9f1ad
KumarAmitt <amitt2015@outlook.com>
2021-08-06 14:36:13 +0530

    Fix poetry card height

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,29 +29,33 @@ const Home = () => {
     return <Error />;
   }
 
+  console.log(isPoetriesLoading);
   return (
     <>
       <div className="home">
         <main>
-          {
-            isPoetriesLoading ? <Loading /> : (
-              poetries.map((p) => (
-                <div key={uniqid()} className="card">
-                  <div className="card-body">
-                    <p>{p.author}</p>
-                    <h3>{p.title.length > 77 ? `${p.title.substr(0, 73)}...` : p.title}</h3>
+          <div className="main-tiles">
+            {
+              isPoetriesLoading ? <Loading /> : (
+                poetries.map((p) => (
+                  <div key={uniqid()} className="card">
+                    <div className="card-body">
+                      <p>{p.author}</p>
+                      <h3>{p.title.length > 77 ? `${p.title.substr(0, 73)}...` : p.title}</h3>
+                    </div>
+                    <div className="card-footer">
+                      <Link to={`/poetry/${p.author}/${p.title}`} className="link">Read Poetry</Link>
+                      <p className="lineCount">
+                        Lines Count:
+                        {p.linecount}
+                      </p>
+                    </div>
                   </div>
-                  <div className="card-footer">
-                    <Link to={`/poetry/${p.author}/${p.title}`} className="link">Read Poetry</Link>
-                    <p className="lineCount">
-                      Lines Count:
-                      {p.linecount}
-                    </p>
-                  </div>
-                </div>
-              ))
-            )
-          }
+                ))
+              )
+            }
+          </div>
+
         </main>
 
         <Filter filterAuthor={handleAuthorChange} />

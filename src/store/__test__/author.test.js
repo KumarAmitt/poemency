4ba71175913c3d033a0dc4967fe830acf3d451e8
KumarAmitt <amitt2015@outlook.com>
2021-08-06 22:50:15 +0530

    Add test for loadAuthor action

--- src/store/__test__/author.test.js
+++ src/store/__test__/author.test.js
@@ -1,11 +1,44 @@
-// import configureAppStore from '../configureStore';
-// import { loadAuthors } from '../slicers/author';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import configureAppStore from '../configureStore';
+import { loadAuthors } from '../slicers/author';
 
 describe('authorSlice', () => {
-  it('should handle the loadTAuthors action', async () => {
-    // const store = configureAppStore();
+  let fakeAxios;
+  let store;
 
-    // await store.dispatch(loadAuthors());
-    // console.log(store.getState().entities.author.authors);
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureAppStore();
+  });
+
+  const authorSlice = () => store.getState().entities.author;
+
+  // const createState = () => ({
+  //   entities: {
+  //     author: {
+  //       authors: [],
+  //     },
+  //   },
+  // });
+
+  describe('loadingAuthors', () => {
+    describe('loadAuthors', () => {
+      it('should return an array of all the authors', async () => {
+        fakeAxios.onGet('/author').reply(200, { authors: ['a1', 'a2'] });
+
+        await store.dispatch(loadAuthors());
+
+        expect(authorSlice().authors).toHaveLength(2);
+      });
+    });
+  });
+
+  it('should not returns authors if Network error found', async () => {
+    fakeAxios.onGet('/author').reply(500);
+
+    await store.dispatch(loadAuthors());
+
+    expect(authorSlice().authors).toHaveLength(0);
   });
 });
